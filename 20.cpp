0e977950adefc2b2df92ace8d657680f28c27c8b
liaoziqian <ziqianliao@gmail.com>
2018-08-02 23:59:03 +0800

    resolved problem 20

---
+++ 20.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (st.empty()) {
+                st.push(s[i]);
+                continue;
+            }
+            if (s[i] == ')' && st.top() == '(') {
+                st.pop();
+            } else if (s[i] == ']' && st.top() == '[') {
+                st.pop();
+            } else if (s[i] == '}' && st.top() == '{') {
+                st.pop();
+            } else {
+                st.push(s[i]);
+            }
+        }
+        return st.empty();
+    }
+};
