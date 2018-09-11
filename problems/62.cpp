390ebfa34c0ca9e5db9415b2064e288908449a82
liaoziqian <ziqianliao@gmail.com>
2018-09-11 22:05:25 +0800

    resolved problem 62

---
+++ problems/62.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if (m == 0 || n == 0) return 0;
+        int dp[m][n];       
+        int i, j;
+        for (i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (j = 0; j < n; j++) {
+            dp[0][j] = 1;            
+        }
+        for (i = 1; i < m; i++) {
+            for (j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
